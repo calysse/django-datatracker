d59d48ae883b9093ce5acb79daa85ea22ba3e9f3
Sebastien Claeys <sebclaeys@gmail.com>
2016-09-01 14:56:12 +0200

    Processing slack tasks async

--- conf.py
+++ conf.py
@@ -4,7 +4,8 @@ DT_MIXPANEL_FORWARD = getattr(settings, 'DT_MIXPANEL_FORWARD', False)
 DT_INTERCOM_FORWARD = getattr(settings, 'DT_INTERCOM_FORWARD', False)
 DT_LOCAL_SAVE = getattr(settings, 'DT_LOCAL_SAVE', False)
 
-DT_MIXPANEL_TOKEN = "7d737864ce0c6cb65d071e8b09c74fc6"
+DT_MIXPANEL_TOKEN = getattr(settings, 'DT_MIXPANEL_TOKEN', False)
+
 DT_INTERCOM_APP_ID = "eiildktn"
 DT_INTERCOM_API_KEY = "0186775356d7f3b7e27cac57009ada5326f49c84"
 
