0d65705a1b92a1ff96de0bfff139fcbd38464b44
Sebastien Claeys <sebclaeys@gmail.com>
2017-06-02 14:00:01 +0200

    adding migrations

---
+++ migrations/0003_auto_20170602_1159.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('datatracker', '0002_auto_20170602_1005'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='group',
+            field=models.CharField(default=None, max_length=64, null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
