e7d778edc4338ae058b80c6481d8117963ac71e1
jpocalan <jpocalan@gmail.com>
2019-07-29 16:37:26 +0200

    Adding create/update lead Adding task to allow lead conversion

--- tasks.py
+++ tasks.py
@@ -46,8 +46,22 @@ def intercom_update_company(company_id, name, plan, custom_attributes, monthly_s
     intercom = conf.intercom_client
     intercom.companies.create(company_id=company_id, name=name, plan=plan, custom_attributes=custom_attributes, monthly_spend=monthly_spend)
 
+
 @task
 def intercom_update_user(user_id, name, email, phone, signed_up_at, custom_attributes, companies):
     intercom = conf.intercom_client
     intercom.users.create(user_id=user_id, name=name, email=email, phone=phone, signed_up_at=signed_up_at, custom_attributes=custom_attributes, companies=companies)
 
+
+@task
+def intercom_update_lead(name, email, custom_attributes):
+    intercom = conf.intercom_client
+    return intercom.leads.create(name=name, email=email, custom_attributes=custom_attributes)
+
+
+@task
+def intercom_convert_lead(lead_id, user_id):
+    intercom = conf.intercom_client
+    intercom_user = intercom.users.find(user_id=user_id)
+    intercom_lead = intercom.leads.find(id=lead_id)
+    return intercom.leads.convert(intercom_lead, intercom_user)
